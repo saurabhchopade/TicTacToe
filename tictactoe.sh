3be3463ae98729d1e0473858547f8bfb61f37a6b
saurabh chopade <saurabhchopade3250@gmail.com>
2020-06-11 12:21:19 +0530

    Feature: Board design template

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,4 +1,45 @@
+
 #!/bin/bash -x
-
 echo "Welcome to tic Tac Toe GAME";
+#Locic For Board Design 
+#Assumed for 1-2-3
+a="O";
+b="O";
+c="O";
+#Assumed for 4-5-6
+d="X";
+e="X";
+f="X";
+#Assumed for 7-8-9
+h="X";
+i="X";
+j="X";
 
+echo "-------------------------";
+
+echo -ne "|       ";echo -ne "|       ";echo -ne "|       ";echo -ne "|       "; echo -e " ";
+
+echo -ne "|   $a   ";echo -ne "|   $b   ";echo -ne "|   $c   ";echo -ne "|       "; echo -e " ";
+
+echo -ne "|       ";echo -ne "|       ";echo -ne "|       ";echo -ne "|       "; echo -e " ";
+
+echo "-------------------------";
+
+echo -ne "|       ";echo -ne "|       ";echo -ne "|       ";echo -ne "|       "; echo -e " ";
+
+echo -ne "|   $d   ";echo -ne "|   $e   ";echo -ne "|   $f   ";echo -ne "|       "; echo -e " ";
+
+echo -ne "|       ";echo -ne "|       ";echo -ne "|       ";echo -ne "|       "; echo -e " ";
+
+
+echo "-------------------------";
+
+echo -ne "|       ";echo -ne "|       ";echo -ne "|       ";echo -ne "|       "; echo -e " ";
+
+echo -ne "|   $h   ";echo -ne "|   $i   ";echo -ne "|   $j   ";echo -ne "|       "; echo -e " ";
+
+echo -ne "|       ";echo -ne "|       ";echo -ne "|       ";echo -ne "|       "; echo -e " ";
+
+echo "-------------------------";
+
+echo -ne " ";
