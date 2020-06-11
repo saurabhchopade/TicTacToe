e7f150acf3264a7c4f5320e4dea8edad1c530496
saurabh chopade <saurabhchopade3250@gmail.com>
2020-06-11 20:46:45 +0530

    Modified: winning Condition

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,24 +1,40 @@
 
-
 #!/bin/bash -x
 echo "Welcome to tic Tac Toe GAME";
 #CONSTANTS
+
+#This is for To check who PLAY FIRST  
 COMPUTERPLAY=0;
 PLAYERPLAY=1;
+
+#Board Symbols
 SYMBOL1="O";
 SYMBOL2="X";
+
+#100 means player and 1 means Computer
 PLAYERVALUE=100;
 COMPUTERVALUE=1;
 GAMEINIT=1;
+
+#To check pair Addition// if addition equal to this Pair means Winning Condition 
+COMPUTERPAIR=3;
+PLAYERPAIR=300;
+
 #VARIABLES
-tossResult="-";
-computerSymbol="-";
-playerSymbol="-";
+
+tossResult=" ";
+computerSymbol=" ";
+playerSymbol=" ";
+#To Switch The players
 switchFlag=1;
 
 #ARRAY
+#gameStorage To Actual Values
+#symbolStorage for To store symbols  By mapping Two players Values
 declare -a gameStorage;
 declare -a symbolStorage;
+
+#Board function display the Design with respective Symbols
 	function board() {
 		#Locic For Board Design 
 		#Assumed for 1-2-3
@@ -68,11 +84,14 @@ declare -a symbolStorage;
 #Toss for Who Play First
 #This Logic Run Only Starting of The GAME
 	function assignSymbol(){
-		blank="-";
+
+		blank=0;
+		dummySymbol=" ";
 		#Array Initialized Because to set Boad Alignment Proper
 		for (( index=0;index<=10;index++))
       do
-         symbolStorage[(( index ))]="$blank";
+         symbolStorage[(( index ))]="$dummySymbol";
+			gameStorage[(( index ))]="$blank";
       done
 
 		tossResult=$((RANDOM%2));
@@ -85,38 +104,57 @@ declare -a symbolStorage;
   	 		if [ $symbol -eq $COMPUTERPLAY ]
    		then
 				printf "\n";
+				#IF WE ASSIGN SYMBOL TO Computer IT WILL NOT ASSIGNE SYMBOL TO PLAYER
+            #SO that we assigned PLAYER Symbol
       		computerSymbol=$SYMBOL1;
+				playerSymbol=$SYMBOL2;
 				echo "COMPUTER SYMBOL IS="$computerSymbol;
+				printf "\n";
    		else
 				printf "\n";
+				#IF WE ASSIGN SYMBOL TO Computer IT WILL NOT ASSIGNE SYMBOL TO PLAYER
+            #SO that we assigned PLAYER Symbol
       		computerSymbol=$SYMBOL2;
+				playerSymbol=$SYMBOL1;
 				echo "COMPUTER SYMBOL IS="$computerSymbol;
+				printf "\n";
    		fi;
 
 			else
 				#Player Choose The Symbol
 				#If player pressed Wrong Choice Then Player Automatically Get the Symbol --X--
 
-				echo "Player Play First";
+				echo "YOU Play First";
+				printf "\n \n";
 				echo "To Choose Symbol Press Number";
 				echo "1) O";
 				echo "2) X";
+				printf "\n \n";
 				read -p "Enter=" choice;
 
 				if [ $choice -eq 1 ]
       		then
 					printf "\n";
+					#IF WE ASSIGN SYMBOL TO PLAYER IT WILL NOT ASSIGNE SYMBOL TO COMPUTER 
+					#SO that we assigned computer Symbol
         			playerSymbol=$SYMBOL1;
-        			echo "PLAYER SYMBOL IS="$playerSymbol;
+					computerSymbol=$SYMBOL2;
+        			echo "YOUR SYMBOL IS="$playerSymbol;
+					printf "\n";
       		else
 					printf "\n";
+					#IF WE ASSIGN SYMBOL TO PLAYER IT WILL NOT ASSIGNE SYMBOL TO COMPUTER
+					#SO that we assigned computer Symbol
          		playerSymbol=$SYMBOL2;
-					echo "PLAYER SYMBOL IS="$playerSymbol;
+					computerSymbol=$SYMBOL1;
+					echo "YOUR  SYMBOL IS="$playerSymbol;
+					printf "\n";
       		fi;
 			fi;
 		#Calling Board Function  To enter Valid cells
       board;
 	}
+
 
 #THIS FUNCTION FOR INSERTING VALUE AND THE SYMBOL TO ARRAY 
 #VALUE ARRAY(GAMESTORAGE) WE USING FOR THE WINNING CONDITION AND FOR BLOCKING THE PLAYER USING THE VALUES
@@ -144,7 +182,37 @@ declare -a symbolStorage;
 
 	}
 
+#HERE WE ARE CHECKING WINNING CONDITION
+	function checkWin() {
+		whichPlayer=$1
+		if [ $PLAYERPLAY -eq $whichPlayer ]
+		then
+		#PLAYERPAIR=300;
+		whoIs=$PLAYERPAIR;
+		else
+		#$COMPUTERPAIR=3;
+		whoIs=$COMPUTERPAIR;
+		fi;
+		#GameStorage Contains These Values
+		#	PLAYERVALUE=100;
+		#	COMPUTERVALUE=1;
+		pair1=$(( ${gameStorage[1]} + ${gameStorage[2]} +  ${gameStorage[3]}  ));
+		pair2=$(( ${gameStorage[1]} + ${gameStorage[4]} +  ${gameStorage[7]}  ));
+		pair3=$(( ${gameStorage[1]} + ${gameStorage[5]} +  ${gameStorage[9]}  ));
+		pair4=$(( ${gameStorage[2]} + ${gameStorage[5]} +  ${gameStorage[8]}  ));
+		pair5=$(( ${gameStorage[4]} + ${gameStorage[5]} +  ${gameStorage[6]}  ));
+		pair6=$(( ${gameStorage[3]} + ${gameStorage[5]} +  ${gameStorage[7]}  ));
+		pair7=$(( ${gameStorage[3]} + ${gameStorage[6]} +  ${gameStorage[9]}  ));
+		pair8=$(( ${gameStorage[7]} + ${gameStorage[8]} +  ${gameStorage[9]}  ));
 
+		if [ $pair1 -eq  $whoIs ] || [ $pair2 -eq  $whoIs ] || [ $pair3 -eq  $whoIs ] || [ $pair4 -eq  $whoIs ] || [ $pair5 -eq  $whoIs ] || [ $pair6 -eq  $whoIs ] || [ $pair7 -eq  $whoIs ] || [ $pair8 -eq  $whoIs ] 
+		then
+			echo "WINNER WINNER CHIKEN DINNER";
+			exit 1;
+		fi;
+	}
+
+#THIS IS MAIN
 #Player Switching Done Here
 #This Is main
 	while :
@@ -160,24 +228,31 @@ declare -a symbolStorage;
 	#THIS LOGIC FOR SWITCHING PLAYERS
 	if [ $switchFlag -eq 1 ]
 	then
-		echo "==PLAYER TURN==";
+		printf "\n ";
+		echo "     YOUR TURN";
+		echo "      Symbol=" $playerSymbol;
 
+		printf "\n \n";
 		#Cell Choice From User
 		read -p  "Enter VALID CELL=" cell;
 
 		#calling to insert into array
      	insertInStorage $cell $PLAYERVALUE;
-
-      #To Reflect in Board
+		
+      #Board changes here
       board;
-
+		checkWin $PLAYERPLAY;
       switchFlag=0;
-
+		printf "\n \n ";
 	else
-		echo "==COMPUTER TURN==";
-
+		printf "\n ";
+		echo "     COMPUTER TURN";
+		echo "      Symbol=" $computerSymbol;
+		board;
 		switchFlag=1;
-
+		
 	fi;
 
 	done;
+unset gameStorage;
+unset symbolStorage;
